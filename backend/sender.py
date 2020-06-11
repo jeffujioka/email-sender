4848e23c3e95a8a89e9b9e5f3311b46afedda14d
Jefferson M Fujioka <jefferson.fujioka@gmail.com>
2020-06-11 10:50:47 +0200

    feat: use environment variables for custom settings

--- backend/sender.py
+++ backend/sender.py
@@ -1,5 +1,6 @@
 import redis
 import json
+import os
 import psycopg2
 from bottle import Bottle, request
 
@@ -8,8 +9,24 @@ class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres password=postgres host=email-sender-db'
+        redis_host = os.getenv('ENV_ESENDER_REDIS_HOSTNAME', 'queue')
+        redis_port = int(os.getenv('ENV_ESENDER_REDIS_PORT', 6379))
+
+        print(f'ENV_ESENDER_REDIS_HOSTNAME={redis_host} -> ', type(redis_host))
+        print(f'ENV_ESENDER_REDIS_PORT={redis_port} -> ', type(redis_port))
+        self.queue = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+
+        db_hostname = os.getenv('ENV_ESENDER_DB_HOSTNAME', 'email-sender-db')
+        db_name = os.getenv('ENV_ESENDER_DB_NAME', 'email_sender')
+        user = os.getenv('ENV_ESENDER_DB_USER', 'postgres')
+        password = os.getenv('ENV_ESENDER_DB_PASSWORD', 'postgres')
+        
+        print(f'ENV_ESENDER_DB_HOSTNAME={db_hostname}')
+        print(f'ENV_ESENDER_DB_NAME={db_name}')
+        print(f'ENV_ESENDER_DB_USER={user}')
+        print(f'ENV_ESENDER_DB_PASSWORD={password}')
+
+        DSN = f'dbname={db_name} user={user} password={password} host={db_hostname}'
         self.conn = psycopg2.connect(DSN)
 
     def register_message(self, subject, message):
--- worker/worker.py
+++ worker/worker.py
@@ -1,10 +1,13 @@
 import redis
 import json
+import os
 from time import sleep
 from random import randint
 
 if __name__ == "__main__":
-    r = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('ENV_ESENDER_REDIS_HOSTNAME', 'queue')
+    redis_port = int(os.getenv('ENV_ESENDER_REDIS_PORT', 6379))
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
     print('Waiting for messages...')
     while True:
         # msg = {'subject': 'Jefferson'}
